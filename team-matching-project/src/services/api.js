00032c3af4e029cd0185f01667a50f6d0b598412
ecsyscmmtasy <nmabov@gmail.com>
2026-06-18 15:03:34 +0500

    feat: Создал API сервиса и верстка формы регистрации

--- team-matching-project/src/services/api.js
+++ team-matching-project/src/services/api.js
@@ -3,5 +3,9 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'http://localhost:8000/api',
 });
+//функция для регистрации
+export const registerUser = (UserData) => api.post('register/', userData);
 
-export default function api;
+export const loginUser = (UserData) => api.post('login/', userData);
+
+export default api;
